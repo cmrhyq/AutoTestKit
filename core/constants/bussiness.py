e93e4697470ce800ce676242d8502bd4b3d6ba66
AlanHuang <1678336204@qq.com>
2026-08-11 17:59:28 +0800

    added sandbox menu enum values

--- core/constants/bussiness.py
+++ core/constants/bussiness.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 from attr import dataclass
 
-
+# ================== 菜单枚举 ==================
 @dataclass
 class SystemMenu(str, Enum):
     """
@@ -19,7 +19,6 @@ class SandboxMenu(str, Enum):
         - 沙箱管理
         - 运维看板
     沙箱集群：
-        - 集群管理
         - 节点管理
     镜像管理：
         - 镜像库管理
@@ -29,14 +28,32 @@ class SandboxMenu(str, Enum):
     SDK使用示例：
         - SDK使用示例
     """
-    SANDBOX_MANAGER = "沙箱管理"
+    SANDBOX_MANAGE = "沙箱管理"
     MAINTENANCE_DASHBOARD = "运维看板"
     SANDBOX_CLUSTER = "沙箱集群"
-    CLUSTER_MANAGER = "集群管理"
-    NODE_MANAGER = "节点管理"
-    IMAGE_MANAGER = "镜像管理"
-    IMAGE_LIBRARY_MANAGER = "镜像库管理"
-    TEMPLATE_MANAGER = "模板管理"
+    CLUSTER_MANAGE = "集群管理"
+    NODE_MANAGE = "节点管理"
+    IMAGE_MANAGE = "镜像管理"
+    IMAGE_LIBRARY_MANAGE = "镜像库管理"
+    TEMPLATE_MANAGE = "模板管理"
     BUILD_RECORD = "构建记录"
     SDK_EXAMPLE = "SDK使用示例"
 
+# ================== 页面Frame 枚举 ==================
+@dataclass
+class SandboxFramePath(str, Enum):
+    """
+    沙箱访问Frame Path
+    SANDBOX_MANAGE = 沙箱管理
+    NODE_MANAGE = 节点管理
+    IMAGE_LIBRARY_MANAGE = 镜像库管理
+    TEMPLATE_MANAGE = 模板管理
+    BUILD_RECORD = 构建记录
+    SDK_EXAMPLE = SDK使用示例
+    """
+    SANDBOX_MANAGE = "/iframe/sandbox-web/admin/sandboxManage"
+    NODE_MANAGE = "/iframe/sandbox-web/cluster/node"
+    IMAGE_LIBRARY_MANAGE = "/iframe/sandbox-web/admin/imageManage"
+    TEMPLATE_MANAGE = "/iframe/sandbox-web/admin/templateManage"
+    BUILD_RECORD = "/iframe/sandbox-web/templates/builds1"
+    SDK_EXAMPLE = "/iframe/sandbox-web/sdk/examples1"
